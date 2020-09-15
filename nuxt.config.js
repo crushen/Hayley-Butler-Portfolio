0bea2902b75a73ed2fc4956a14c6fa96a1c0c419
Charlotte Rushen <charlotterushen@hotmail.com>
2020-09-15 17:10:59 +0100

    Added SCSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,13 +9,19 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@600&display=swap' }
     ]
+  },
+  modules: [
+    '@nuxtjs/style-resources',
+  ],
+  styleResources: {
+    scss: [ '~/assets/styles/variables.scss', '~/assets/styles/main.scss' ]
   },
   css: [],
   plugins: [],
   components: true,
   buildModules: [],
-  modules: [],
   build: {}
 }
