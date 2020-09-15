56ed466c4856d49a820a5c2fbf7068587813d985
Charlotte Rushen <charlotterushen@hotmail.com>
2020-09-15 18:50:35 +0100

    Added collections

---
+++ components/sections/Collections.vue
@@ -0,0 +1,23 @@
+<template>
+  <section id="collections" class="section-padding whole-screen">
+    <div class="content">
+      <div class="gallery-title">
+        <h2>COLLECTIONS</h2>
+        <div class="line" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+#collections {
+  background: $light-font;
+  color: $background;
+}
+</style>
